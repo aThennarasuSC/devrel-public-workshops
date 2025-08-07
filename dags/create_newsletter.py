6dd36e24b73fe3bbe70ee8a05bbdf2ef6af3c797
aThennarasuSC <96258425+aThennarasuSC@users.noreply.github.com>
2025-08-07 15:51:49 -0500

    Update create_newsletter.py

--- dags/create_newsletter.py
+++ dags/create_newsletter.py
@@ -45,4 +45,56 @@ def selected_quotes(context: dict):
     raw_zen_quotes.remove(median_quote)
 
     short_quote = next(quote for quote in raw_zen_quotes if int(quote["c"]) < median)
-    long_quote = next(quote for quote in raw_zen_quotes_
+    long_quote = next(quote for quote in raw_zen_quotes if int(quote["c"]) > median)
+
+    run_date = context["triggering_asset_events"][Asset("raw_zen_quotes")][0].extra[
+        "run_date"
+    ]
+    yield Metadata(Asset("selected_quotes"), {"run_date": run_date})
+
+    return {
+        "median_q": median_quote,
+        "short_q": short_quote,
+        "long_q": long_quote,
+    }
+
+
+@asset(schedule=[selected_quotes])
+def formatted_newsletter(context: dict):
+    """
+    Formats and stores the newsletter using the selected quotes.
+    """
+    object_storage_path = ObjectStoragePath(
+        f"{OBJECT_STORAGE_SYSTEM}://{OBJECT_STORAGE_PATH_NEWSLETTER}",
+        conn_id=OBJECT_STORAGE_CONN_ID,
+    )
+
+    selected_quotes = context["ti"].xcom_pull(
+        dag_id="selected_quotes",
+        task_ids="selected_quotes",
+        key="return_value",
+        include_prior_dates=True,
+    )
+
+    run_date = context["triggering_asset_events"][Asset("selected_quotes")][0].extra[
+        "run_date"
+    ]
+
+    newsletter_template_path = object_storage_path / "newsletter_template.txt"
+    newsletter_template = newsletter_template_path.read_text()
+
+    newsletter = newsletter_template.format(
+        quote_text_1=selected_quotes["short_q"]["q"],
+        quote_author_1=selected_quotes["short_q"]["a"],
+        quote_text_2=selected_quotes["median_q"]["q"],
+        quote_author_2=selected_quotes["median_q"]["a"],
+        quote_text_3=selected_quotes["long_q"]["q"],
+        quote_author_3=selected_quotes["long_q"]["a"],
+        date=run_date,
+    )
+
+    date_newsletter_path = object_storage_path / f"{run_date}_newsletter.txt"
+    date_newsletter_path.write_text(newsletter)
+
+    yield Metadata(Asset("formatted_newsletter"), {"run_date": run_date})
+    return newsletter
